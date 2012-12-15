1a67bc00b8f7a6f68ae8901a918a46c8ec8282c5
minsler <minsler@vm-lubuntu.(none)>
2012-12-15 20:10:14 +0300

    Added mapping with component

---
+++ src/main/java/by/minsler/hibernate/bean/Book.java
@@ -0,0 +1,9 @@
+package by.minsler.hibernate.bean;
+
+/**
+ * User: minsler
+ * Date: 12/15/12
+ * Time: 7:54 PM
+ */
+public class Book {
+}
---
+++ src/main/java/by/minsler/hibernate/bean/Pen.java
@@ -0,0 +1,9 @@
+package by.minsler.hibernate.bean;
+
+/**
+ * User: minsler
+ * Date: 12/15/12
+ * Time: 7:51 PM
+ */
+public class Pen {
+}
